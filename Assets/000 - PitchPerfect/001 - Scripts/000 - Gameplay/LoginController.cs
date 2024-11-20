5650675d181907107a542f3bbc05495d97a6f497
HeckerCreatives <34593098+Ganielle@users.noreply.github.com>
2024-11-20 08:10:22 +0800

    poosh

---
+++ Assets/000 - PitchPerfect/001 - Scripts/000 - Gameplay/LoginController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class LoginController : MonoBehaviour
+{
+    [SerializeField] private GameObject loadingObj;
+    
+    [Space]
+    [SerializeField] private TMP_InputField usernameTMP;
+    [SerializeField] private TMP_InputField passwordTMP;
+
+    //public void 
+}
